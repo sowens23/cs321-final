beb53bc0e90f49af4e1dc725b77e633475255070
sowens23 <thespencerowens@outlook.com>
2025-04-16 11:16:12 -0800

    init: first commit

---
+++ shell.cpp
@@ -0,0 +1,12 @@
+// Group Members: Ivy Swenson, Spencer Baysinger, Kolby, and Jack D.
+// File Name: shell.cpp
+// Date: 2025/04/16
+// Description: This file contains the implementation of the shell class, which is responsible for managing the shell's command line interface. 
+//              It handles user input, command execution, and output display. The shell class also manages the history of commands entered by 
+//              the user and provides functionality for navigating through the command history.
+
+int main() {
+  // Empty main
+
+  return 0;
+}
